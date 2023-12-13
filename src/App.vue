366b8bea904fc41c872ec536aac7a333b119754e
Saifur Rahman Rubayet <saifur.rubayet@northsouth.edu>
2023-12-13 12:35:53 +0600

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="app">
-    <h1>hello</h1>
+    <h2>Pomodoro</h2>
+    <h3>Press start to begin</h3>
+    <div class="container">
+      <h1>{{ formattedTime(time) }}</h1>
+      <h3 @click="startTime">START</h3>
+    </div>
   </div>
 
 </template>
@@ -13,12 +18,75 @@ export default defineComponent({
   name: 'App',
   components: {},
   setup(){
-    const time= ref("")
+    const time= ref(1500)
+    const interval= ref<number|undefined>(undefined)
+
+    const deduceTime=() =>{
+      if(time.value>0){
+        time.value--
+        clearInterval(interval.value)
+        
+      }
+      // else if(time.value===0){
+      //   console.log("hello");
+        
+      //   clearInterval(interval.value)
+      //   // notifSound()
+      //   time.value= 300
+      // }
+      
+    }
+    const startTime=() =>{
+      interval.value= setInterval(deduceTime, 1000)
+
+    }
+    
+     //F:\Web_ Development\Vue_Firebase\pomorodoapp\sound\notif01.mp3
+    // const notifSound= ()=>{
+    //   const popSound = new Audio('yellow.mp3');
+    //   popSound.play();
+
+    // }
+
+    const formattedTime= (time: number)=>{
+      const min= Math.floor(time/60)
+      const sec= time% 60
+
+      return `${String(min).padStart(2,'0')} : ${String(sec).padStart(2,'0')}`
+    }
+
+    return {time, startTime, formattedTime}
 
   }
 });
 </script>
 
 <style>
+body{
+  
+  text-align: center;
+  position: absolute;
+  align-items: center;
+  justify-content: center;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.container{ 
+  margin: 0 auto;
+  position: relative;
+  width: 400px;
+  height: 400px;
+  border-radius: 50%;
+  border: 4px solid #717171;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #FF6347;
+  font-size: 30px;
+  font-family: Helvetica;
+ 
+}
 
 </style>
